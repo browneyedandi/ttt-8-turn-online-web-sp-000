a24538ee7622d33fcebdf36569ac49727d12a5ec
Andriana Brockman <andriana.brockman@gmail.com>
2020-07-28 21:53:55 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -39,7 +39,7 @@ end
    puts "Please enter 1-9:"
    user_input = gets.chomp
    index = input_to_index(user_input)
-   until valid_move?(board, index) 
+   if valid_move?(board, index) 
    turn(board)
    break
    move(board, index, value = "X")
